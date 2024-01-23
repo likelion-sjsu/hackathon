c51c6ab04c110792ea472b08788f9cd56e217b67
Jooyul Yoon <juyeolyoon@gmail.com>
2024-01-22 19:56:46 -0800

    add attributes result, end, category to room model

--- server/room/models.py
+++ server/room/models.py
@@ -4,8 +4,10 @@ import random
 
 # Create your models here.
 class Room(models.Model):
-    #result = models.TextField()
     code = models.CharField(max_length=22, unique=True)
+    category = models.CharField(max_length=10, default="food")
+    result = models.CharField(max_length=10000, default="")
+    end = models.BooleanField(default=False)
 
     @staticmethod
     def generate_short_identifier(digits=4):
