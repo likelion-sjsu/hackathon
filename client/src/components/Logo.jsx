02ac091ca783133f2aa43440a5bb319cbb0a39e8
Jooyul Yoon <juyeolyoon@gmail.com>
2024-01-25 01:04:20 -0800

    remove git cache

--- client/src/components/Logo.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import LogoSvg from "../assets/logo.svg";
-
-export default function Logo() {
-  return <img src={LogoSvg} alt="Logo" />;
-}
--- client/src/components/TabBar.jsx
+++
@@ -1,3 +0,0 @@
-export default function TabBar() {
-  return null;
-}
