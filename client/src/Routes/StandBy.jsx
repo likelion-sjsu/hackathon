aee2604c3cd8a05d18204f4ba3b9e751652249c8
Jooyul Yoon <juyeolyoon@gmail.com>
2024-01-25 02:54:24 -0800

    add styles on standby page

--- client/src/Routes/StandBy.jsx
+++ client/src/Routes/StandBy.jsx
@@ -1,29 +1,106 @@
-import React from "react";
+import { SERVER_URL, getRoomData } from "api";
+import React, { useEffect, useState } from "react";
+import { useQuery } from "react-query";
 import { useNavigate } from "react-router-dom";
-import styled from "styled-components";
+import styled, { useTheme } from "styled-components";
+import { CircularProgressbar } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
 
-const Container = styled.div``;
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  p {
+    margin-top: 30px;
+  }
+`;
+
+const ClosePollBtn = styled.button`
+  margin-top: 50px;
+  background-color: ${(props) => props.theme.brandColor};
+  font-size: ${(props) => props.theme.fontBtn.fontSize};
+  font-weight: ${(props) => props.theme.fontBtn.fontWeight};
+  color: white;
+  border-radius: 16px;
+  border: none;
+  outline: none;
+  width: 360px;
+  height: 48px;
+`;
+
+const Ring = styled.div`
+  width: 160px;
+  height: 160px;
+  margin-top: 210px;
+  margin-bottom: 30px;
+`;
 
 export default function StandBy() {
+  const theme = useTheme();
   const navigate = useNavigate();
-  const roomInfo = JSON.parse(localStorage.getItem("roomInfo"));
-  console.log(roomInfo);
+  const { code, role, category } = JSON.parse(localStorage.getItem("roomInfo"));
+  const [percent, setPercent] = useState(0);
+  const [periods, setPeriods] = useState(".");
 
-  // room data 5초 가져오기 (몇명이 답했는지, 끝났는지 계속 확인하기)
-  // 끝났으면 /result 로 리디렉트
+  const { data, isLoading } = useQuery(
+    ["room", code],
+    () => getRoomData(code),
+    {
+      onSuccess: async (data) => {
+        if (data.end === true) {
+          const res = await fetch(
+            `${SERVER_URL}/recommend/result/${category}/${code}`,
+            { method: "POST" }
+          );
+          const result = await res.json();
+          console.log(res.status, result);
+          if (res.ok) navigate("/result", { state: result });
+        }
+      },
+      refetchInterval: 5000,
+    }
+  );
 
-  const onclick = () => {
-    // 서버에 끝났다고 알려줘
-    // 10초 뒤에 db에서 삭제해야하는데...
-    navigate("/result");
+  const onclick = async () => {
+    const res = await fetch(`${SERVER_URL}/room/${code}`, { method: "PUT" });
+    const data = await res.json();
+    console.log(res.status, data);
   };
+
+  useEffect(
+    () => data && setPercent((data.answered_count / data.max_count) * 100),
+    [data]
+  );
+
+  useEffect(() => {
+    const delayedFunction = () => {
+      if (periods === "...") {
+        setPeriods(".");
+        return;
+      }
+      setPeriods((prev) => prev + ".");
+    };
+    const timeoutId = setTimeout(delayedFunction, 1000);
+    return () => clearTimeout(timeoutId);
+  }, [periods]);
 
   return (
     <Container>
-      <div>다른 사람들 기다려.</div>
-      <div>현재 13명이 답변했어</div>
-      {roomInfo.role === "leader" && (
-        <button onClick={onclick}>결과 볼래?</button>
+      <Ring>
+        <CircularProgressbar
+          value={percent}
+          text={isLoading ? "-" : data.answered_count + " of " + data.max_count}
+          styles={{
+            path: { stroke: theme.brandColor },
+            text: { fill: theme.mainFont },
+          }}
+        />
+      </Ring>
+      <p>Please wait.</p>
+      <p>We are collecting all the responses{periods}</p>
+      {role === "leader" && (
+        <ClosePollBtn onClick={onclick}>Close Poll</ClosePollBtn>
       )}
     </Container>
   );
