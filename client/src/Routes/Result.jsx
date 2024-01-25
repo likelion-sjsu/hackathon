1aace2fd7036b423011c88a9a018151449be397c
Jooyul Yoon <juyeolyoon@gmail.com>
2024-01-25 00:56:03 -0800

    show result

--- client/src/Routes/Result.jsx
+++ client/src/Routes/Result.jsx
@@ -1,3 +1,19 @@
+import React from "react";
+import { useLocation } from "react-router-dom";
+import styled from "styled-components";
+
+const Container = styled.main`
+  display: grid;
+  place-content: center;
+  height: 100vh;
+`;
+
 export default function Result() {
-  return null;
+  const location = useLocation();
+  const { result } = location.state;
+  return (
+    <Container>
+      <h1>{result}</h1>
+    </Container>
+  );
 }
