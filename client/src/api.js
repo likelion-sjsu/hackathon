428c186d943fc139fcd3e15ae9479ad3ee95040b
Jooyul Yoon <juyeolyoon@gmail.com>
2024-01-26 02:44:31 -0800

    apply server url

--- client/src/api.js
+++ client/src/api.js
@@ -1,4 +1,6 @@
-export const SERVER_URL = "http://127.0.0.1:8000";
+// export const SERVER_URL = "http://127.0.0.1:8000";
+export const SERVER_URL =
+  "https://vscode-adakm.run-us-west2.goorm.site/proxy/8000";
 
 export async function getRoomData(roomId) {
   return (await fetch(`${SERVER_URL}/room/${roomId}`)).json();
