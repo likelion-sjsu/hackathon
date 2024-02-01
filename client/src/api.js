4da3ae11a50d0c3d8311cc1cf870445e8d8829a7
Jooyul Yoon <juyeolyoon@gmail.com>
2024-01-31 16:38:19 -0800

    server port change

--- client/src/api.js
+++ client/src/api.js
@@ -1,4 +1,4 @@
-// export const SERVER_URL = "http://127.0.0.1:8000";
+// export const SERVER_URL = "http://127.0.0.1:8080";
 export const SERVER_URL =
   "https://vscode-adakm.run-us-west2.goorm.site/proxy/8000";
 
